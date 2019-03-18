9320e5f4eb46471e6ced717f02062d0d15447898
macos <macos@macoss-Mac.local>
2019-03-18 08:48:49 -0700

    Initial Commit

---
+++ table view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  table view
+//
+//  Created by macos on 2019-03-18.
+//  Copyright © 2019 macos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
